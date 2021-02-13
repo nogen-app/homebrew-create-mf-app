9091b06fe57442c55944ad67ac244ebe8863a5d1
rareinator <morten.9ssen@gmail.com>
2021-02-13 21:44:11 +0100

    formula v0.1.0

--- create-mf-app.rb
+++ create-mf-app.rb
@@ -2,18 +2,18 @@ require 'rbconfig'
 class CreateMfApp < Formula
   desc ""
   homepage "https://github.com/nogen/create-mf-app"
-  version "{{.Version}}"
+  version "0.1.0"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/nogen/create-mf-app/releases/download/v{{.Version}}/{{.Mac64.FileName}}"
-      sha256 "{{.Mac64.Hash}}"
+      url "https://github.com/nogen/create-mf-app/releases/download/v0.1.0/create-mf-app_0.1.0_darwin_amd64.zip"
+      sha256 "2333f63ec58ac76d8132575f851ca1f9aefebfce4a1c82ab8d87d42c341d9eb3"
     when /linux/
-      url "https://github.com/nogen/create-mf-app/releases/download/v{{.Version}}/{{.Linux64.FileName}}"
-      sha256 "{{.Linux64.Hash}}"
+      url "https://github.com/nogen/create-mf-app/releases/download/v0.1.0/create-mf-app_0.1.0_linux_amd64.tar.gz"
+      sha256 "ce1d6e7d98577cf8d232f1456e26f3ccd46ce4660e0573be936b9c05e255c118"
     when /solaris|bsd/
       :unix
     else
@@ -26,8 +26,8 @@ class CreateMfApp < Formula
     when /darwin|mac os/
       :mac
     when /linux/
-      url "https://github.com/nogen/create-mf-app/releases/download/v{{.Version}}/{{.Linux386.FileName}}"
-      sha256 "{{.Linux386.Hash}}"
+      url "https://github.com/nogen/create-mf-app/releases/download/v0.1.0/create-mf-app_0.1.0_linux_386.tar.gz"
+      sha256 "e1789a78a4983330a5548a69181d90256a669c694192ac4a2a4210d2606f307a"
     when /solaris|bsd/
       :unix
     else
